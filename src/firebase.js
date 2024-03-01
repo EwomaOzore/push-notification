13bea46150092755bd3ef2410216a4f00f864c28
Ewoma Ozore <ewomaozore@gmail.com>
2024-03-01 11:38:35 +0100

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
@@ -14,7 +24,7 @@ export const fetchToken = (setTokenFound) => {
         } else {
             console.log('No registration token available. Request permission to generate one.');
             setTokenFound(false);
-            // shows on the UI that permission is required 
+            // shows on the UI that permission is required
         }
     }).catch((err) => {
         console.log('An error occurred while retrieving token. ', err);
